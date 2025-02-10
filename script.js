70463ccce00474b8245dc82db06b91876a86fbb3
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-02-10 10:31:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,51 @@
 //complete this code
-class Animal {}
+class Animal {
+    constructor(species) {
+        this.species = species;
+    }
 
-class Dog extends Animal {}
+    get speciesName() {
+        return this.species;
+    }
 
-class Cat extends Animal {}
+    makeSound() {
+        console.log(`The ${this.species} makes a sound`);
+    }
+}
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+class Cat extends Animal {
+    constructor() {
+        super('Cat');
+    }
+
+    purr() {
+        console.log('purr');
+    }
+}
+
+class Dog extends Animal {
+    constructor() {
+        super('Dog');
+    }
+
+    bark() {
+        console.log('woof');
+    }
+}
+
+document.getElementById('create-animal-button').addEventListener('click', function() {
+    const animal = new Animal('Lion');
+    animal.makeSound();
+});
+
+document.getElementById('create-cat-button').addEventListener('click', function() {
+    const cat = new Cat();
+    cat.makeSound();
+    cat.purr();
+});
+
+document.getElementById('create-dog-button').addEventListener('click', function() {
+    const dog = new Dog();
+    dog.makeSound();
+    dog.bark();
+});
