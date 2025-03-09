4bfa76e15b765145dfe702509dad3d8ee5588cff
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-03-09 14:32:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,34 @@
+//complete this code
 class Animal {
-    constructor(species) {
+    constructor(species){
         this._species = species;
     }
 
-    get species() {
-        return this._species;
+    get species(){
+        return `${this._species}`;
     }
 
-    makeSound() {
-        console.log(`The ${this.species} makes a sound`);
+    makeSound(){
+        console.log(`The ${this._species} makes a sound`);
     }
 }
 
 class Cat extends Animal {
-    constructor(species) {
-        super(species);
-    }
-
-    purr() {
+    purr(){
+        // return 'purr';
         console.log('purr');
+        
     }
 }
 
 class Dog extends Animal {
-    constructor(species) {
-        super(species);
-    }
-
-    bark() {
+    bark(){
+        // return 'woof';
         console.log('woof');
     }
 }
 
-document.getElementById('create-cat-button').addEventListener('click', function() {
-    const cat = new Cat('Siamese');
-    cat.makeSound();
-    cat.purr();
-});
-
-document.getElementById('create-dog-button').addEventListener('click', function() {
-    const dog = new Dog('Golden Retriever');
-    dog.makeSound();
-    dog.bark();
-});
+// Do not change the code below this line
+window.Animal = Animal;
+window.Dog = Dog;
+window.Cat = Cat;
